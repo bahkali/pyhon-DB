f2727ffe38d1223c70514c247565772c2298ee26
kaly <kalimamadou@gmail.com>
2020-12-27 00:24:22 -0800

    Explore SQLAlchemy crud operation

--- flask_app.py
+++ flask_app.py
@@ -13,12 +13,28 @@ class Persons(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column( db.String(), nullable=False)
 
-    # def __init__(self, name):
-    #     self.name = name
+    def __init__(self, name):
+        self.name = name
 
+    def __repr__(self):
+        return f'<Person ID: {self.id}, name: {self.name}>'
 
 db.create_all()
 
+#Persons.query.all()
+#Persons.query.count()
+#Persons.query.filter_by(name='Amy')
+#Persons.query.filter(Persons.name == 'Amy')
+#Persons.query.get(1) <- gets by primary key
+
+
+#db.session.query(Person) <=> Person.query
+#session.query(Person).join(Team)
+
+#db.session.add(person)
+#db.session.add_all([person1, person2])
+#db.session.rollback()
+#db.session.commit()
 
 @app.route('/')
 def index():
@@ -26,6 +42,5 @@ def index():
     return 'Hello, ' + person.name
 
 
-if __name__ == "__main__":
-    
+if __name__ == "__main__":  
     app.run(debug=True)
